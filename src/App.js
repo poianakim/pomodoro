e51d1e5fea9d83fb1ee3b6ece802969d4e9a4dea
kby906 <kby906@gmail.com>
2020-11-22 20:08:45 +0100

    modified setInterval with useEffect

--- src/App.js
+++ src/App.js
@@ -1,79 +1,94 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 const App = () => {
-
-  const [breakTime, setBreakTime] = useState(1);
-  const [sessionTime, setSessionTime] = useState(1);
+  const [breakTime, setBreakTime] = useState(5);
+  const [sessionTime, setSessionTime] = useState(25);
   const [timerType, setTimerType] = useState("SESSION");
   const [timeLeft, setTimeLeft] = useState(sessionTime * 60);
   const [pause, setPause] = useState(true);
   const starter = useRef(null);
-
+  
+  const changeTime = () => {
+    if (timeLeft > 0) {
+      setTimeLeft((timeLeft) => timeLeft - 1);
+    } 
+    if (timeLeft === 0) {
+      if (timerType === "SESSION") {
+        console.log(timeLeft);
+        setTimerType("BREAK");
+        setTimeLeft(breakTime * 60);
+        buzzer();
+      } else {
+        setTimeLeft(sessionTime * 60);
+        setTimerType("SESSION");
+        buzzer();
+      }
+    }
+  };
+  useEffect(() => {
+    if(!pause){
+      const interval = setInterval(changeTime, 1000)
+      return () => clearInterval(interval)
+          }
+  })
   const onBreakDecreClick = () => {
-    setBreakTime(breakTime - 1)
-    if (timerType === "BREAK") {
-      setTimeLeft((breakTime-1) * 60)
+    if (breakTime > 0) {
+      setBreakTime(breakTime - 1);
+      if (timerType === "BREAK") {
+        setTimeLeft((breakTime - 1) * 60);
+      }
+    } else {
+      return;
     }
-  }
+  };
   const onBreakIncreClick = () => {
-    setBreakTime(breakTime + 1)
+    setBreakTime(breakTime + 1);
     if (timerType === "BREAK") {
-      setTimeLeft((breakTime-1) * 60)
+      setTimeLeft((breakTime + 1) * 60);
     }
-  }
+  };
   const onSessDecreClick = () => {
-    setSessionTime(sessionTime => sessionTime - 1)
-    if (timerType === "SESSION") {
-      setTimeLeft((sessionTime -1) * 60)
+    if (sessionTime > 0) {
+      setSessionTime((sessionTime) => sessionTime - 1);
+      if (timerType === "SESSION") {
+        setTimeLeft((sessionTime - 1) * 60);
+      }
+    } else {
+      return;
     }
-  }
+  };
   const onSessIncreClick = () => {
-    setSessionTime(sessionTime => sessionTime + 1)
+    setSessionTime((sessionTime) => sessionTime + 1);
     if (timerType === "SESSION") {
-      setTimeLeft((sessionTime+1) * 60)
+      setTimeLeft((sessionTime + 1) * 60);
     }
-  }
+  };
   const buzzer = () => {
     const sound = document.getElementById("beep");
     sound.play();
-  }
-  const changeTime = () => {
-    if (timeLeft > 0) {
-      setTimeLeft(timeLeft => timeLeft - 1)
-    } else if (timeLeft === 0 && timerType === "SESSION") {
-      setTimeLeft(breakTime * 60)
-      setTimerType("BREAK")
-      buzzer()
-    } else if (timeLeft === 0 && timerType === "BREAK") {
-      setTimeLeft(sessionTime * 60)
-      setTimerType("SESSION")
-      buzzer()
-    }
-  }
+  };
   const startTimer = () => {
-    console.log(timerType)
+    console.log(sessionTime, breakTime, timeLeft);
     setPause(false);
-    starter.current =  setInterval(changeTime, 100)
-  }
-    const stopTimer = () => {
-    setPause(true)
-    clearInterval(starter.current)
-  }
+  };
+  const stopTimer = () => {
+    setPause(true);
+    clearInterval(starter.current);
+  };
   const onResetClick = () => {
-    stopTimer()
-    setBreakTime(5)
-    setSessionTime(25)
-    setTimerType("SESSION")
-    setTimeLeft(1500)
-  }
+    stopTimer();
+    setBreakTime(5);
+    setSessionTime(25);
+    setTimerType("SESSION");
+    setTimeLeft(1500);
+  };
   const onChangePause = () => {
-    console.log(sessionTime, breakTime, timeLeft)
     if (pause) {
-      startTimer()
+      startTimer();
     } else {
-      stopTimer()
+      stopTimer();
     }
-  }
+  };
   const toMMSS = () => {
     var minutes = Math.floor(timeLeft / 60);
     var seconds = timeLeft - minutes * 60;
@@ -84,45 +99,40 @@ const App = () => {
       seconds = "0" + seconds;
     }
     return minutes + ":" + seconds;
-  }
+  };
   return (
-    <div id='clock'>
+    <div id="clock">
       <h1>🍅Pomodoro Clock🍅</h1>
       <div>
         <h5>BREAK LENGTH</h5>
-        <input
-          onClick={onBreakDecreClick}
-          type="button" value="-" />
+        <input onClick={onBreakDecreClick} type="button" value="-" />
         <h4>{breakTime}</h4>
-        <input
-          onClick={onBreakIncreClick}
-          type="button" value="+" />
+        <input onClick={onBreakIncreClick} type="button" value="+" />
       </div>
       <div>
         <h5>SESSION LENGTH</h5>
-        <input
-          onClick={onSessDecreClick}
-          type="button" value="-" />
+        <input onClick={onSessDecreClick} type="button" value="-" />
         <h4>{sessionTime}</h4>
-        <input
-          onClick={onSessIncreClick}
-          type="button" value="+" />
+        <input onClick={onSessIncreClick} type="button" value="+" />
       </div>
       <div className="session-section">
         <div>
           <h3>{timerType}</h3>
           <h4>{toMMSS(timeLeft)}</h4>
-          <button
-            onClick={onChangePause} id="start-pause">{pause ? "START" : "PAUSE"}</button>
-          <button onClick={onResetClick} id="reset">RESET</button>
+          <button onClick={onChangePause} id="start-pause">
+            {pause ? "START" : "PAUSE"}
+          </button>
+          <button onClick={onResetClick} id="reset">
+            RESET
+          </button>
         </div>
       </div>
       <audio
-          id="beep"
-          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
-        />
+        id="beep"
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
